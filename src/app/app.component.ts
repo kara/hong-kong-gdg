877dd16f215fd86ba2fcd5a22fe73b5a38ef045c
Kara Erickson <karakara@google.com>
2016-12-20 19:59:48 +0800

    commit from gdg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import {MdDialog, MdDialogRef} from "@angular/material";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'app works!';
+
+  dogs = [
+    {name: 'Mal', img: 'Mal.png'},
+    {name: 'Porter', img: 'Porter.png'},
+    {name: 'Koby', img: 'Koby.png'},
+    {name: 'Razzle', img: 'Razzle.png'},
+    {name: 'Molly', img: 'Molly.png'},
+    {name: 'Husi', img: 'Husi.png'},
+  ];
+
+  constructor(public dialog: MdDialog) {}
+
+  addDog() {
+    const dialogRef = this.dialog.open(AddDialogComponent);
+    dialogRef.afterClosed().first().subscribe((result) => {
+      this.dogs.push({name: result, img: 'Porter.png'});
+    });
+  }
+
+}
+
+@Component({
+  selector: 'add-dialog',
+  template: `
+    <md-input placeholder="Name" [(ngModel)]="name"></md-input>
+    <button md-raised-button color="accent" (click)="submit()">ADD</button>
+  `
+})
+export class AddDialogComponent {
+  name: string;
+
+  constructor(public dialogRef: MdDialogRef<AddDialogComponent>) {}
+
+  submit() {
+    this.dialogRef.close(this.name);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
+
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { AppComponent, AddDialogComponent } from './app.component';
+import {MaterialModule} from "@angular/material";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddDialogComponent
+  ],
+  entryComponents: [
+    AppComponent,
+    AddDialogComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
